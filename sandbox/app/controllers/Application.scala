0063b63dac7dce5bec7512e1aa930123e6d72503
misogi <ryudo@misogi.info>
2012-12-13 10:38:23 +0900

    play 2.0.4+windows で動作確認

--- sandbox/app/controllers/Application.scala
+++ sandbox/app/controllers/Application.scala
@@ -4,12 +4,9 @@ import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
-
+  
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-
-  def list = Action {
-    Ok("hoge");
-  }
+  
 }
--- sandbox/project/Build.scala
+++ sandbox/project/Build.scala
@@ -1,21 +1,18 @@
 import sbt._
 import Keys._
-import play.Project._
+import PlayProject._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "sandbox"
-  val appVersion      = "1.0-SNAPSHOT"
+    val appName         = "sandbox"
+    val appVersion      = "1.0-SNAPSHOT"
 
-  val appDependencies = Seq(
-    // Add your project dependencies here,
-    jdbc,
-    anorm
-  )
+    val appDependencies = Seq(
+      // Add your project dependencies here,
+    )
 
-
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
-  )
+    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+      // Add your own project settings here      
+    )
 
 }
