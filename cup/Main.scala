dba8b504e403c34e18dbbf71b272e26c454234a9
misogi <ryudo@misogi.info>
2012-12-14 09:32:42 +0900

    sbtに対応した

---
+++ cup/Main.scala
@@ -0,0 +1,6 @@
+object Main{
+  def main(args: Array[String]) {
+    val ae = new ArrayElement(Array("Hello", "World"))
+    println(ae.width)
+  }
+}
--- main.scala
+++
@@ -1,2 +0,0 @@
-val ae = new ArrayElement(Array("Hello", "World"))
-ae.width
