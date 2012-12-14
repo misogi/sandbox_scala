690b55f4a8d93421fbec9447431136a78c52ebe9
misogi <dev@misogi.info>
2012-12-14 16:35:12 +0900

    例外のテスト

--- cup/src/test/scala/ElementSuite.scala
+++ cup/src/test/scala/ElementSuite.scala
@@ -4,6 +4,12 @@ import Element.elem
 class ElementSuite extends FunSuite {
   test("elem result should have passed width") {
     val ele = elem('x', 2, 3)
-    assert(ele.width == 2)
+    expect(2){
+      ele.width
+    }
+
+    //intercept[IllegalArgumentException] {
+    //  elem('x', -2, -5)
+    //}
   }
 }
