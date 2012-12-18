2b04d710f36da8877831b5855eb6dc5e9d59c868
misogi <dev@misogi.info>
2012-12-18 10:49:53 +0900

    get/set の例を追加

---
+++ cup/src/main/scala/BankAccount.scala
@@ -0,0 +1,27 @@
+class BankAccount {
+  private var bal: Int = 0
+
+  def balance: Int = bal
+
+  def deposit(amount: Int) {
+    require (amount > 0)
+    bal += amount
+  }
+
+  def withdraw(amount: Int): Boolean =
+    if (amount > bal) false
+    else {
+      bal -= amount
+      true
+    }
+}
+
+
+class Thermometer {
+  var celsius: Float = _
+  def fahrenheit = celsius * 9 / 5 + 32
+  def fahrenheit_= (f: Float) {
+    celsius = (f - 32) * 5/ 9
+  }
+  override def toString = fahrenheit + "F/" + celsius + "C"
+}
