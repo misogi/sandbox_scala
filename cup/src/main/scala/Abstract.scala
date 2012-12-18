897a23da1f893a88008e34adcabb1b55df8ccff5
misogi <dev@misogi.info>
2012-12-18 15:04:43 +0900

    抽象型、抽象パラメタなどの勉強

--- cup/src/main/scala/Abstract.scala
+++ cup/src/main/scala/Abstract.scala
@@ -1,3 +1,4 @@
+// chapter 20 abstract
 trait Abstract {
   type T
   def transform(x: T): T
@@ -8,4 +9,45 @@ class Concrete extends Abstract {
   type T = String
   def transform(x: String) = x + x
   val initial = "hi"
+}
+
+trait RationalTrait {
+  val numerArg: Int
+  val denomArg: Int
+  lazy val g = {
+    require(denomArg != 0)
+    gcd(numerArg, denomArg)
+  }
+
+  override def toString = numerArg + "/" + denomArg
+
+  private def gcd(a: Int, b: Int): Int = if(b == 0) a else gcd(b, a%b)
+}
+
+object Demo {
+  lazy val x = {println("initializing x"); "done"}
+}
+
+class Food
+abstract class Animal {
+  type SuitableFood <: Food
+  def eat(food: SuitableFood)
+}
+
+class Grass extends Food
+class Fish extends Food
+class Cow extends Animal {
+  type SuitableFood = Grass
+  //  c.eat(new c.SuitableFood) // OK
+  //  c.eat(new Grass) // OK
+  //  c.eat(new Food ) //NG
+  override def eat(food: Grass) {}
+}
+
+class Pasture {
+  var animals: List[Animal {type SuitableFood = Grass}] = Nil
+}
+
+object Color extends Enumeration {
+  val Red, Green , Blue = Value
 }
