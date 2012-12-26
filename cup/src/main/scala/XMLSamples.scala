7bc8f6edbcdbd8622cd02a30ab840171d6ad9893
misogi <dev@misogi.info>
2012-12-26 15:42:59 +0900

    XMLのシリアライズとデシリアライズ

---
+++ cup/src/main/scala/XMLSamples.scala
@@ -0,0 +1,66 @@
+// chapter 28
+object XMLSamples {
+  def test = {
+    val hello = <a> {"hello"+"world"}</a>
+    val yearMade = 1955
+    val years = <a> {if (yearMade < 2000) <old>{yearMade}</old> else xml.NodeSeq.Empty} </a>
+    val secure = <a>{"<b>test</b>"}</a>
+    val insecure = "<a>" + "</a>security hole<a>"+"</a>"
+    println(hello)
+    println(years)
+    println(secure)
+    println(insecure)
+    println(hello.text)
+    println(secure.text)
+
+    val hell = <a><b><c>hell!</c></b></a>
+    println(hell \ "a")
+    println(hell \ "b")
+    println(hell \ "c")
+    println("-----")
+    println(hell \\ "a")
+    println(hell \\ "b")
+    println(hell \\ "c")
+
+    val joe = <employee name="Joe" />
+    println(joe \ "@name")
+  }
+
+  abstract class CCTherm {
+    val description: String
+    val yearMade: Int
+    val dateObtained: String
+    val bookPrice: Int
+    val purchasePrice: Int
+    val condition: Int
+    override def toString = description
+    def toXML =
+      <cctherm>
+        <description>{description}</description>
+        <yearMade>{yearMade}</yearMade>
+        <dateObtained>{dateObtained}</dateObtained>
+        <bookPrice>{bookPrice}</bookPrice>
+        <purchasePrice>{purchasePrice}</purchasePrice>
+        <condition>{condition}</condition>
+      </cctherm>
+
+    def fromXML(node: scala.xml.Node): CCTherm = new CCTherm {
+      val description = (node \ "description").text
+      val yearMade = (node \ "yearMade").text
+      val dateObtained = (node \ "dateObtained").text
+      val bookPrice = (node \ "bookPrice").text
+      val purchasePrice = (node \ "purchasePrice").text
+      val condition = (node \ "condition").text
+    }
+  }
+
+  def coke = new CCTherm {
+    val description = "hot dog #5"
+    val yearMade = 1952
+    val dateObtained = "March 14, 2006"
+    val bookPrice = 2199
+    val purchasePrice = 500
+    val condition = 9
+  }
+
+}
