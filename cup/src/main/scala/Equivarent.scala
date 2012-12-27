b5e35ad3bc7931d6d0a5fe6c613c921b072359df
misogi <dev@misogi.info>
2012-12-27 18:07:45 +0900

    add color class

--- cup/src/main/scala/Equivarent.scala
+++ cup/src/main/scala/Equivarent.scala
@@ -1,6 +1,20 @@
-class Point(val x: Int, val y: Int) {
+class Point(var x: Int, val y: Int) {
+  override def hashCode = 41 * (41 + x) + y
   override def equals(other: Any): Boolean = other match {
-    case that: Point => this.x == that.x && this.y == that.y
+    case that: Point => this.x == that.x && this.y == that.y &&
+      this.getClass == that.getClass
+    case _ => false
+  }
+}
+
+object Colord extends Enumeration {
+  val Red, Orange, Yellow, Green, Blue, Indigo, Violet = Value
+}
+
+class ColoredPoint(x: Int, y: Int, val color: Colord.Value)
+  extends Point(x, y) {
+  override def equals(other: Any) = other match {
+    case that: ColoredPoint => this.color == that.color && super.equals(that)
     case _ => false
   }
 }
