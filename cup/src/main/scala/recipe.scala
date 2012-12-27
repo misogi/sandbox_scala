efbcb4f992327e42ed83615ae46262bfa9fbf4a9
misogi <dev@misogi.info>
2012-12-27 17:08:32 +0900

    モジュール化のレッスン

--- cup/src/main/scala/recipe.scala
+++ cup/src/main/scala/recipe.scala
@@ -78,3 +78,17 @@ object SimpleBrowser extends Browser {
 object StudentBrowser extends Browser {
   val database = StudentDatabase
 }
+
+object GotApples {
+  def main(args: Array[String]) {
+    val db: Database =
+      if(args(0) == "student") StudentDatabase
+      else SimpleDatabase
+    object browser extends Browser {
+      val database: db.type = db
+    }
+    val apple = SimpleDatabase.foodNamed("Apple").get
+    for(recipe <- browser.recipesUsing(apple)) println(recipe)
+    for(category <- db.allCategories) browser.displayCategory(category)
+  }
+}
